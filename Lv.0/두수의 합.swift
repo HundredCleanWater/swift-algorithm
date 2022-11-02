b515c329b1982f9d6c42700b3741634bd169a49a
HundredCleanWater <maxmp9904@gmail.com>
2022-11-02 13:34:28 +0900

    두수의 합

---
+++ Lv.0/두수의 합.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+//정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    var sum = num1 + num2
+    return sum
+}
