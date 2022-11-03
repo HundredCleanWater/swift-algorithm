cf6137e5bc122852833b3a861bcae7722032c5dd
HundredCleanWater <maxmp9904@gmail.com>
2022-11-03 21:19:11 +0900

    두 수의 차

---
+++ Lv.0/두 수의 차.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+//정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    var subtraction = num1 - num2
+    return subtraction
+}
