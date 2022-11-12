9e91920d15a3ab7b1c075c00a39784d4b5d2cc0b
HundredCleanWater <maxmp9904@gmail.com>
2022-11-12 21:09:21 +0900

    옷가게 할인받기

---
+++ Lv.0/옷가게 할인받기/옷가게 할인받기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ price:Int) -> Int {
+
+    guard price < 500000 else {
+        return Int(Double(price)*0.8)
+    }
+    guard  price < 300000 else {
+        return Int(Double(price) * 0.9)
+    }
+    guard price < 100000 else {
+    return Int(Double(price) * 0.95)
+    }
+    return price
+}
